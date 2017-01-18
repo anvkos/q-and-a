afddadb6cf3610783c1f51c9d1dde27d210c3526
Anton Kosolapov <anvkos@ya.ru>
2017-01-18 21:26:56 +0700

    +: 24 add model Answer, associated with Question

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  it { should validate_presence_of :title }
-  it { should validate_presence_of :body }
+  describe 'validation' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :body }
+  end
+
+  describe 'association' do
+    it { should have_many(:answers).dependent(:destroy) }
+  end
 end
