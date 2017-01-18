5af383201de2a67207bbcc588fcedec071d8b5d6
Anton Kosolapov <anvkos@ya.ru>
2017-01-18 22:22:42 +0700

    *: 24 change the order of tests for models

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
+  describe 'association' do
+    it { should have_many(:answers).dependent(:destroy) }
+  end
+
   describe 'validation' do
     it { should validate_presence_of :title }
     it { should validate_presence_of :body }
-  end
-
-  describe 'association' do
-    it { should have_many(:answers).dependent(:destroy) }
   end
 end
