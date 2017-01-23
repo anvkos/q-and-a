b16dbf5f39553bfd19285ecdc94db60d04846fea
Anton Kosolapov <anvkos@ya.ru>
2017-01-23 21:52:17 +0700

    +: 26 guest can registred, auth user logout

---
+++ spec/acceptance/user/sign_out_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'User sign out', %q{
+  In order to complete the work of the session
+  As a user
+  I want to be able to go out and the system
+} do
+
+  given(:user) { create(:user) }
+
+  scenario 'Authenticated user try to sign out' do
+    sign_in(user)
+    click_on 'Logout'
+    expect(page).to have_content 'Signed out successfully.'
+    expect(current_path).to eq root_path
+  end
+end
---
+++ spec/acceptance/user/sign_up_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'User sign up', %q{
+  In order to be able to ask questions
+  As a guest
+  I want to be able to register in the system
+} do
+
+  scenario 'Guest user try to sign up' do
+    visit root_path
+    click_link 'Sign up'
+    fill_in 'Email', with: 'reguser@test.com'
+    fill_in 'Password', with: '123456'
+    fill_in 'Password confirmation', with: '123456'
+    click_button 'Sign up'
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+    expect(current_path).to eq root_path
+  end
+end
