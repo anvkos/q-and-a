50fa4ee4df99ae724bf410d4d2e152fd2e7e649e
Anton Kosolapov <anvkos@ya.ru>
2017-01-26 21:52:10 +0700

    +: 26 test for #author?

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,23 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :email }
     it { should validate_presence_of :password }
   end
+
+  describe '#author?' do
+    it 'returns true' do
+      user_id = rand(1..100)
+      user = create(:user, id: user_id)
+      entity = double('Entity')
+      allow(entity).to receive_messages(user_id: user_id)
+      expect(user.author?(entity)).to be_truthy
+    end
+
+    it 'returns false' do
+      user_id = rand(1..100)
+      other_id = rand(200..300)
+      user = create(:user, id: user_id)
+      entity = double('Entity')
+      allow(entity).to receive_messages(user_id: other_id)
+      expect(user.author?(entity)).to be_falsey
+    end
+  end
 end
