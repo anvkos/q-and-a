07b7f8f80bf819d68b46af4cf896dbb9f66b280c
Anton Kosolapov <kosolapov@netpost.ru>
2017-01-31 16:20:54 +0700

    +: 28 show validation errors when creating Answer

--- spec/acceptance/answers/create_answer_spec.rb
+++ spec/acceptance/answers/create_answer_spec.rb
@@ -20,16 +20,28 @@ feature 'Create answer', %q{
     expect(current_path).to eq question_path(question)
   end
 
-  scenario 'Authenticated user create answer with invalid attributes', js: true  do
-    sign_in(user)
-    visit question_path(question)
-    answer_text = 'text567'
-    fill_in 'Body', with: answer_text
-    click_on 'Add new answer'
-    expect(page).not_to have_content answer_text
+  describe 'Authenticated user create answer with invalid attributes' do
+    before do
+      sign_in(user)
+      visit question_path(question)
+    end
+
+    scenario 'body text is too short', js: true do
+      answer_text = 'text567'
+      fill_in 'Body', with: answer_text
+      click_on 'Add new answer'
+      expect(page).not_to have_content answer_text
+      expect(page).to have_content 'Body is too short'
+    end
+
+    scenario 'body text is blank', js: true do
+      fill_in 'Body', with: ''
+      click_on 'Add new answer'
+      expect(page).to have_content "Body can't be blank"
+    end
   end
 
-  scenario 'Non-authenticated user try to creates qiestion', js: true  do
+  scenario 'Non-authenticated user try to creates qiestion', js: true do
     visit question_path(question)
 
     expect(page).not_to have_selector('#answer_body')
