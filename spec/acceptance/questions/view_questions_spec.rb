600cad592a732762c5b4a2c9c82e109eea1d90be
Anton Kosolapov <kosolapov@netpost.ru>
2017-01-31 14:13:08 +0700

    +: 28 acceptanse helper

--- spec/acceptance/questions/view_questions_spec.rb
+++ spec/acceptance/questions/view_questions_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require_relative '../acceptance_helper'
 
 feature 'View questions', %q{
   In order to read the text of the question
--- spec/acceptance/user/sign_out_spec.rb
+++ spec/acceptance/user/sign_out_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require_relative '../acceptance_helper'
 
 feature 'User sign out', %q{
   In order to complete the work of the session
