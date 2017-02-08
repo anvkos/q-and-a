d384169dea14ef3e73a3ac93e474a28aee7a0ec6
Anton Kosolapov <kosolapov@netpost.ru>
2017-02-08 15:06:21 +0700

    +: 29 User can add several files to Answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,7 +5,7 @@ class Answer < ApplicationRecord
 
   validates :body, presence: true, length: { minimum: 10 }
 
-  accepts_nested_attributes_for :attachments
+  accepts_nested_attributes_for :attachments, reject_if: :all_blank, allow_destroy: true
 
   scope :first_best, -> { order('best DESC') }
 
--- spec/acceptance/answers/add_files_to_answer_sprec.rb
+++ spec/acceptance/answers/add_files_to_answer_sprec.rb
@@ -5,7 +5,6 @@ feature 'Add files to answer', %q{
   As an answer's author
   I'd like to be able to attach files
 } do
-
   given(:user) { create(:user) }
   given(:question) { create(:question) }
 
@@ -21,6 +20,20 @@ feature 'Add files to answer', %q{
 
     within '.answers' do
       expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+    end
+  end
+
+  scenario 'User adds several files', js: true do
+    fill_in 'Body', with: 'My answer text'
+    click_on 'Add file'
+    inputs = all('input[type="file"]')
+    inputs[0].set("#{Rails.root}/spec/spec_helper.rb")
+    inputs[1].set("#{Rails.root}/public/favicon.ico")
+    click_on 'Add new answer'
+
+    within '.answers' do
+      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+      expect(page).to have_link 'favicon.ico', href: '/uploads/attachment/file/2/favicon.ico'
     end
   end
 end
