21417f8c3503f63f2af160a60d9cc33c46607c46
Anton Kosolapov <kosolapov@netpost.ru>
2017-02-08 14:14:59 +0700

    +: 29 User can add several files to Question

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ gem 'devise'
 
 gem 'carrierwave', '~> 1.0'
 gem 'remotipart'
+gem 'cocoon'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- spec/acceptance/questions/add_files_to_question_spec.rb
+++ spec/acceptance/questions/add_files_to_question_spec.rb
@@ -5,10 +5,9 @@ feature 'Add files to question', %q{
   As an question's author
   I'd like to be able to attach files
 } do
-
   given(:user) { create(:user) }
 
-  background do
+  before do
     sign_in(user)
     visit new_question_path
   end
@@ -20,5 +19,18 @@ feature 'Add files to question', %q{
     click_on 'Create'
 
     expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+  end
+
+  scenario 'User adds several files when asks question', js: true do
+    fill_in 'Title', with: 'Test question'
+    fill_in 'Body', with: 'text question'
+    click_on 'Add file'
+    inputs = all('input[type="file"]')
+    inputs[0].set("#{Rails.root}/spec/spec_helper.rb")
+    inputs[1].set("#{Rails.root}/public/favicon.ico")
+    click_on 'Create'
+
+    expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+    expect(page).to have_link 'favicon.ico', href: '/uploads/attachment/file/2/favicon.ico'
   end
 end
