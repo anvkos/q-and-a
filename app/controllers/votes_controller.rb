e584ed6ea11b583eaeab7e54195f17b37070c16e
Anton Kosolapov <anvkos@ya.ru>
2017-02-18 14:12:23 +0700

    *: 30 votable type of url

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -42,8 +42,9 @@ class VotesController < ApplicationController
   end
 
   def set_votable!
-    votable_id = params["#{params[:votable_type].underscore}_id"]
-    @votable = params[:votable_type].constantize.find(votable_id)
+    votable_type = request.fullpath.split('/').second.singularize
+    votable_id = params["#{votable_type}_id"]
+    @votable = votable_type.classify.constantize.find(votable_id)
   rescue NoMethodError, NameError
     render_error(:bad_request, 'Error', 'Not the correct vote data!')
   end
