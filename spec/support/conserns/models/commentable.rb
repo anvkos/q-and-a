382db72dc86279f9c56a2cd55862a409e4293540
Anton Kosolapov <anvkos@ya.ru>
2017-02-24 13:43:47 +0700

    +: 31 concern Commentable

---
+++ spec/support/conserns/models/commentable.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+shared_examples_for 'commentable' do
+  let(:model) { described_class }
+  let(:entity) { create(model.to_s.underscore.to_sym) }
+  let(:user) { create(:user) }
+
+  it { should have_many(:comments) }
+end
