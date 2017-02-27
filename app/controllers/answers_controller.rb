648806506ab4c72b6f911f66e631da92be02156b
Anton Kosolapov <kosolapov@netpost.ru>
2017-02-27 13:24:44 +0700

    *: 32 AnswersController with responders

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -5,26 +5,29 @@ class AnswersController < ApplicationController
 
   after_action :publish_answer, only: [:create]
 
+  respond_to :js
+
   def new
-    @answer = Answer.new
+    respond_with(@answer = Answer.new)
   end
 
   def create
-    @answer = @question.answers.new(answer_params)
-    @answer.user = current_user
-    @answer.save
+    respond_with(@answer = @question.answers.create(answer_params))
   end
 
   def update
     @answer.update(answer_params) if current_user.author?(@answer)
+    respond_with(@answer)
   end
 
   def mark_best
     @answer.mark_best if current_user.author?(@answer.question)
+    respond_with(@answer)
   end
 
   def destroy
     @answer.destroy if current_user.author?(@answer)
+    respond_with(@answer)
   end
 
   private
@@ -38,7 +41,7 @@ class AnswersController < ApplicationController
   end
 
   def answer_params
-    params.require(:answer).permit(:body, attachments_attributes: [:file, :id, :_destroy])
+    params.require(:answer).permit(:body, attachments_attributes: [:file, :id, :_destroy]).merge(user_id: current_user.id)
   end
 
   def publish_answer
