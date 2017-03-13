6efc982437a17a556ae16b8e6fec7aad4d0d5eba
Anton Kosolapov <anvkos@ya.ru>
2017-03-13 19:14:36 +0700

    +: 36 api answers index, show and create actions

---
+++ app/controllers/api/v1/answers_controller.rb
@@ -0,0 +1,34 @@
+class Api::V1::AnswersController < Api::V1::BaseController
+  before_action :set_question, only: [:index, :create]
+  before_action :set_answer, only: [:show]
+
+  authorize_resource
+
+  def index
+    @answers = @question.answers
+    respond_with(@answers, each_serializer: AnswersSerializer)
+  end
+
+  def show
+    respond_with(@answer)
+  end
+
+  def create
+    @answer = @question.answers.create(answer_params.merge(user_id: current_resource_owner.id))
+    respond_with(@answer)
+  end
+
+  private
+
+  def set_answer
+    @answer = Answer.find(params[:id])
+  end
+
+  def set_question
+    @question = Question.find(params[:question_id])
+  end
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
+end
